9e923c8cc940e8828c6eb2849c1fce6d8efe7ace
Malilo <sandramalilo21@gmail.com>
2025-05-29 17:39:40 +0300

    Set up navigation between index, posts, and users pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import Header from "@/components/layout/Header";
 
-const PostsPage = () => (
-  <div className="p-4">
-    <h1 className="text-2xl">Posts Page</h1>
-  </div>
-);
+const Posts: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <main className="p-6">
+        <h2 className="text-3xl font-semibold">Posts Page</h2>
+        <p className="mt-2">This is where your posts will be displayed.</p>
+      </main>
+    </div>
+  );
+};
 
-export default PostsPage;
+export default Posts;
